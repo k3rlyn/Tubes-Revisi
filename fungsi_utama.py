eac9f90d08c5cf635ac875d41fdfb47deefa2332
PanjiSri <116989429+PanjiSri@users.noreply.github.com>
2023-04-17 15:14:23 +0700

    Menambahkan Fungsi Save

--- fungsi_utama.py
+++ fungsi_utama.py
@@ -53,4 +53,64 @@ def kumpul():
 
     print("Jin menemukan", jumlah_pasir,"pasir,",jumlah_batu, "batu, dan",  jumlah_air,"air")
 
+#-----------------------------------------------------------------------------------------------------------------------------------------
+#F14-Fungsi Save
+def save(user, candi, bahan_bangunan):
+
+    #mendapatkan folder parent
+    parent = os.getcwd()
+
+    #menambahkan folder save
+    save_folder = os.path.join(parent, 'save')
+
+    #input nama folder baru
+    input_file = input("Masukkan nama folder: ")
+
+    #path dari folder yang ditambahkan oleh pengguna
+    file_path = os.path.join(save_folder, input_file)
+    
+    #Mengubah array menjadi string
+    string_user = arr_to_string("user.csv",user)
+    string_candi = arr_to_string("candi.csv",candi)
+    string_bahan_bangunan = arr_to_string("bahan_bangunan.csv",bahan_bangunan)
+
+    print("Saving...")
+
+    #Mengecek apakah folder save sudah ada
+    if not (os.path.exists(save_folder)):
+        print("Membuat folder save...")
+
+    #Mengecek apakah folder yang diinputkan sudah ada
+    if os.path.exists(file_path):
+        
+        #Berjalan menelusuri folder
+        for dirpath, dirnames, filenames in os.walk(file_path):
+
+            #Jika kondisi di dalam folder kosong (tidak ada file)
+            if filenames == []:
+                os.chdir(file_path)
+
+                #menulis string ke dalam format.csv
+                write_to_csv(string_user, string_candi, string_bahan_bangunan)
+                print("Berhasil menyimpan data di folder save/{}".format(input_file))
+
+            else:
+
+                #lanjutkan code untuk mengubah file yang lama dengan file yang baru            
+                os.chdir(file_path)
+
+                #menghapus semua file yang ada di folder
+                for i in range(3):
+                    os.remove(filenames[i])
+
+                #menulis string ke dalam format.csv
+                write_to_csv(string_user, string_candi, string_bahan_bangunan)
+                print("Berhasil menyimpan data di folder save/{}".format(input_file))
+    else:
+        os.makedirs(file_path)
+        os.chdir(file_path)
+        write_to_csv(string_user, string_candi, string_bahan_bangunan)
+        print("Membuat folder save/{}".format(input_file))
+        print("Berhasil menyimpan data di folder save/{}".format(input_file))
+
 
